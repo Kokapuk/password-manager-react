76cae0e0ad7a156306ec8cc9bccd11ba32821d6c
Kokapukk <yarik.pavlov.971@gmail.com>
2023-11-22 21:39:49 +0200

    Added custom window controls

--- client/src/components/TitleBar/TitleBar.tsx
+++ client/src/components/TitleBar/TitleBar.tsx
@@ -1,15 +1,37 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { titleBarHeight } from '.';
 import isDesktopApp from '../../utils/isDesktopApp';
 import styles from './TitleBar.module.scss';
 
 const TitleBar = () => {
+  const [isMaximized, setMaximized] = useState(false);
+
+  useEffect(() => {
+    window.electron.ipcRenderer.on('toggleMaximized', (_event: unknown, isMaximized: boolean) => {
+      setMaximized(isMaximized);
+    });
+  }, []);
+
   if (!isDesktopApp()) {
     return null;
   }
 
   return createPortal(
-    <div className={styles.bar} style={{ height: titleBarHeight }} />,
+    <div className={styles.bar}>
+      <button onClick={() => window.electron.ipcRenderer.send('minimize')} className={styles.controlButton}>
+        &#xE921;
+      </button>
+      <button onClick={() => window.electron.ipcRenderer.send('toggleMaximized')} className={styles.controlButton}>
+        {isMaximized ? <>&#xE923;</> : <>&#xE922;</>}
+      </button>
+      <button
+        onClick={() => window.electron.ipcRenderer.send('close')}
+        className={classNames(styles.controlButton, styles.close)}
+      >
+        &#xE8BB;
+      </button>
+    </div>,
     document.getElementById('titleBarPortal') as HTMLElement
   );
 };
--- client/src/components/TitleBar/index.ts
+++ client/src/components/TitleBar/index.ts
@@ -1,3 +1,1 @@
 export { default } from './TitleBar';
-
-export const titleBarHeight = 30;
